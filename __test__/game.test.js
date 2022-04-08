e48d5407a6c7b31859d5cb4869621d5eb695803c
davidpa86 <davidparras@playtomic.io>
2022-04-08 12:50:58 +0200

    remove test

--- __test__/game.test.js
+++ __test__/game.test.js
@@ -1,14 +1,14 @@
 const Game = require('../src/game').default
-const fs = require('fs')
+// const fs = require('fs')
 
-describe('App', () => {
-  it('Contains the compiled JavaScript', async () => {
-    fs.readFile('./public/main.js', 'utf8', (err, data) => {
-      expect(err).toBe(null)
-      expect(data).toMatchSnapshot()
-    })
-  })
-})
+// describe('App', () => {
+//   it('Contains the compiled JavaScript', async () => {
+//     fs.readFile('./public/main.js', 'utf8', (err, data) => {
+//       expect(err).toBe(null)
+//       expect(data).toMatchSnapshot()
+//     })
+//   })
+// })
 
 describe('Game', () => {
   let game, p1, p2
